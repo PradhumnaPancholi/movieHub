d451919cfb74d99abcb26108158565cf1ce85ee3
PradhumnaPancholi <pnpancholi6629.pnp@gmail.com>
2018-06-29 19:57:39 -0400

    added page to display content

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.get('/', function(req, res){
     res.render('home');
 });
 
+//for displaying result//
+app.get('/results', function(req, res){
+    var search = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + search + "&apikey=thewdb";
+    request(url, function(error, response, body){
+        if(!error && responseCode == 200){
+            var data = JSON.parse(body);
+            res.render();
+        }
+    });    
+});
+
 //setting up server//
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('Movie Hub is online');
